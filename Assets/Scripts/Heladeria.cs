3026eae35c6eb8cee5a8084fc0c9776cdbabc47b
MarguJ <margulesjoa@gmail.com>
2025-04-11 09:00:18 -0300

    Entrega Final, Hecho todo
    DIEZZZZZZZZ

--- Assets/Scripts/Heladeria.cs
+++ Assets/Scripts/Heladeria.cs
@@ -4,10 +4,48 @@ using UnityEngine;
 
 public class Heladeria : MonoBehaviour
 {
+    public string gustoAPedir;
+    public float gramosAPedir;
+    float costoPorGramo;
+    float costoFinal;
+    float descuentoFrutilla;
+    float costoDescuento;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        costoPorGramo = 1.25f;
+        descuentoFrutilla = 0.1f;
+        if (gramosAPedir < 250 || gramosAPedir > 3000)
+        {
+            Debug.Log("ERROR. Ingrese una cantidad entre 250g y 3000g");
+            return;
+        }
+        else
+        {
+            if (gustoAPedir == "CHO")
+            {
+                costoFinal = gramosAPedir * costoPorGramo;
+                Debug.Log("Serian $" + costoFinal);
+            }
+            else if (gustoAPedir == "DDL")
+            {
+                costoFinal = gramosAPedir * costoPorGramo;
+                Debug.Log("Serian $" + costoFinal);
+            }
+            else if (gustoAPedir == "FRU")
+            {
+                costoFinal = gramosAPedir * costoPorGramo;
+                costoDescuento = costoFinal - costoFinal * descuentoFrutilla;
+                Debug.Log("Serian $" + costoDescuento);
+            }
+            else
+            {
+                Debug.Log("Solo hay tres gustos: Chocolate (ingrese como CHO), Frutilla (Ingrese como FRU), y Dulce de leche (Ingrese como DDL).");
+                return;
+            }
+
+        }
     }
 
     // Update is called once per frame
